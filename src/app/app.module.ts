92016080c4e055207d2a491bed9c119c66f06ea2
viacheslavbocharov <147165756+viacheslavbocharov@users.noreply.github.com>
2023-12-08 18:53:04 +0000

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthHeaderComponent } from './auth-header/auth-header.component';
+import { AuthFooterComponent } from './auth-footer/auth-footer.component';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthHeaderComponent,
+    AuthFooterComponent,
+    AuthFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth-form/auth-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-form',
+  templateUrl: './auth-form.component.html',
+  styleUrls: ['./auth-form.component.scss'],
+
+})
+export class AuthFormComponent {
+  public inputText = '';
+  public passwordShow = false;
+  //public checkbox = false;
+}
